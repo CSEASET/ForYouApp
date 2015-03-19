126f49bfa1cca88663027c0197e44994d0ce5b67
Vijayant <vijayant.saini123@gmail.com>
2015-03-19 21:13:30 +0530

    ver 1.2 final code dump

--- app/src/main/java/com/helpinghands/foryou/sem_list.java
+++ app/src/main/java/com/helpinghands/foryou/sem_list.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 
 public class sem_list extends ActionBarActivity {
--- app/src/main/java/com/helpinghands/foryou/subject_list.java
+++ app/src/main/java/com/helpinghands/foryou/subject_list.java
@@ -13,7 +13,6 @@ import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.internal.view.menu.ActionMenuItemView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
